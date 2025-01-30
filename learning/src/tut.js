15588f1a64442069a4387ff9ca5abfb3a8df10ca
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-01-31 00:09:54 +0530

    Add initial project setup with dependencies and configuration files

---
+++ learning/src/tut.js
@@ -0,0 +1,18 @@
+let age =24;
+let name = age >18 ? 'Adut' :'child'; // ternary operator
+
+const Component =() =>{
+    return age > 18 ? <div>Adut</div> :<div>Child</div>
+};
+
+//object creation
+const person ={
+    name:"Person",
+    age:20,
+    isMarried:false,
+};d
+
+const Name = person.name;
+const Age = person.age;
+const IsMarried =person.isMarried;
+
