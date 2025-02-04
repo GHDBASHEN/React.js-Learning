5ba7c183eeb3eefd8f38d9ea8d95a449c34ebce8
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-02-04 15:10:24 +0530

    Update TopicBox component to display dynamic topic and ID

--- learning/src/components/TopicBox.js
+++ learning/src/components/TopicBox.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './topicsBox.css';
 export default function TopicBox() {
+    const topic ="My Topic ";
+    const ID =20;
   return (
     <div>
         <div className='topicBox'>
             <span className='text'>
-                This is a Topic
+                This is a {topic} and ID is {ID}
             </span>
         </div>
     </div>
