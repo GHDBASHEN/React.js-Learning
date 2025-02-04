3ff790617cb637a530487c363a00153eadb473dc
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-02-04 15:27:35 +0530

    Update TopicBox component to include alert and array display in the text

--- learning/src/components/TopicBox.js
+++ learning/src/components/TopicBox.js
@@ -7,7 +7,7 @@ export default function TopicBox() {
     <div>
         <div className='topicBox'>
             <span className='text'>
-                This is a {topic} and ID is {ID}
+                This is a {topic} and ID is {ID} and {alert("Hello")} this is a array {[10,20,"saman",10]}
             </span>
         </div>
     </div>
