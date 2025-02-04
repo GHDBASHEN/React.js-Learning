72329967d08bcd17a9581239bb1bb1bace7123f2
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-02-04 19:42:08 +0530

    Add Home and About pages; integrate TopicBox component with dynamic props

--- learning/src/Pages/Home.js
+++ learning/src/Pages/Home.js
@@ -1,14 +1,24 @@
 import React from 'react'
 import Content from '../components/content'
+import TopicBox from '../components/TopicBox';
 export default function Home() {
-  return (
-    <div>
-        
+    return (
+        <div>
+
             <h1>
                 This is Home Page
             </h1>
             <Content />
-    </div>
-  )
+            <TopicBox Food="Kottu" price="200">
+                <span>This is children</span>
+            </TopicBox>
+            <TopicBox Food="Pizza" price="1500">
+                <span>
+                    <button>Click me</button>
+                </span>
+            </TopicBox>
+            <TopicBox Food="Burger" />
+        </div>
+    )
 }
 
