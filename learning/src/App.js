66830f43a6085acace33a016784ffe56c06bdda4
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-02-04 10:04:01 +0530

    Add PartOne component and update App to include it multiple times

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import PartOne from './components/PartOne';
 
 function App() {
   return (
     <div className="App">
       <h1> This is My First React Project</h1>
+      <PartOne /> 
+      <PartOne /> 
+      <PartOne /> 
     </div>
   );
 }
---
+++ learning/src/components/PartOne.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function PartOne() {
+  return (
+    <div>
+        <h1>Hello</h1>
+
+    </div>
+  )
+}
