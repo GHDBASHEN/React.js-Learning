75b3f14fb0869d8d9e53e916b52427129b84261e
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-02-06 23:49:38 +0530

    Update Football1 component to pass event type to shooting alert

--- learning/src/components/Football.js
+++ learning/src/components/Football.js
@@ -3,12 +3,12 @@ import React from 'react'
  
 
 export  const Football1 =() =>{
-    const shoot1 = (a) => {
-      alert(a);
+    const shoot1 = (a,b) => {
+      alert(b.type);
     }
   
     return (
-      <button onClick={()=>shoot1("Goal")}>Take the shot!</button>
+      <button onClick={(event)=>shoot1("Goal",event)}>Take the shot!</button>
     );
   }
 
