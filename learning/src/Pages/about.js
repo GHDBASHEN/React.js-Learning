858c32dc04d1680d5cb9c9fe6a565e8973598181
GHDB ASHEN <115166357+GHDBASHEN@users.noreply.github.com>
2025-02-15 18:17:50 +0530

    Add Sass support and custom styles for the about page

--- learning/src/Pages/about.js
+++ learning/src/Pages/about.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './my_scss.scss';
 
 export default function about() {
   return (
     <div>
         <h1>about</h1>
+        <p>This is about pages</p>
     </div>
   )
 }
